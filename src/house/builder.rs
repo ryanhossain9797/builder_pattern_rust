5c31f07c3dc370dc4d382ca9c49aa7d73f25cacf
Raiyan Hossain <ryanhossain9797@gmail.com>
2021-09-14 18:28:06 +0600

    change privacy controls

--- src/house/builder.rs
+++ src/house/builder.rs
@@ -2,8 +2,34 @@ pub mod stone_builder;
 pub mod wood_builder;
 
 use super::*;
+use std::fmt::Display;
 pub use stone_builder::*;
 pub use wood_builder::*;
+
+pub struct House {
+    material: String,
+    rooms: Vec<Room>,
+    kitchen: Kitchen,
+    pool: Option<Pool>,
+    bathrooms: Vec<Bathroom>,
+}
+
+impl Display for House {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let message = format!(
+            "A house made of {} with {} room(s), {} bathroom(s), a Kitchen of size {} and {}",
+            self.material,
+            self.rooms.len(),
+            self.bathrooms.len(),
+            self.kitchen.size,
+            match &self.pool {
+                Some(_) => "with a pool",
+                _ => "without a pool",
+            }
+        );
+        write!(f, "{}", message)
+    }
+}
 
 pub trait HouseBuilder {
     fn add_rooms(self: Box<Self>, rooms: Vec<Room>) -> Box<dyn HouseBuilder>;
--- src/house/models.rs
+++ src/house/models.rs
@@ -1,5 +1,3 @@
-use std::fmt::Display;
-
 pub struct Pool {
     pub size: i8,
 }
@@ -14,28 +12,4 @@ pub struct Bathroom {
 
 pub struct Room {
     pub size: i8,
-}
-
-pub struct House {
-    pub material: String,
-    pub rooms: Vec<Room>,
-    pub kitchen: Kitchen,
-    pub pool: Option<Pool>,
-    pub bathrooms: Vec<Bathroom>,
-}
-
-impl Display for House {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let message = format!(
-            "A house made of {} with {} room(s), {} bathroom(s) and {}",
-            self.material,
-            self.rooms.len(),
-            self.bathrooms.len(),
-            match &self.pool {
-                Some(_) => "with a pool",
-                _ => "without a pool",
-            }
-        );
-        write!(f, "{}", message)
-    }
 }
